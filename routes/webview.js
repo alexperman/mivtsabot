7933b6cd225bdde5176c30455677bccbcd720922
alexfinastra <alex.perman@gmail.com>
2019-02-11 13:53:16 +0200

    webview stores #2

--- routes/webview.js
+++ routes/webview.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/stores', function(req, res, next) {
 	res.render('stores');
+});
+
+router.get('/categories', function(req, res, next) {
+	res.render('categories');
 });
 
 module.exports = router;
