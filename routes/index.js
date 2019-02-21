ed288f51ce19fe3fb9eb8c2cae85f77f5031f2b0
alexfinastra <alex.perman@gmail.com>
2019-02-21 07:53:28 +0200

    updated backlog #1

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,12 @@ router.get('/', function(req, res, next) {
 	res.render('homepage');
 });
 
+
+router.post('/userinput', function(req, res, next) {
+	console.log("----> request " + JSON.stringify(req.body))
+	res.json({
+  	"redirect_to_blocks": ["Welcome Message"]
+	})
+});
+
 module.exports = router;
