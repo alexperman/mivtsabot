a5ae0952f8d7852876bf69cbe81ab8c9b9adf889
alexperman <alex.perman@gmail.com>
2019-07-10 13:53:53 +0300

    FB integration #2

--- routes/index.js
+++ routes/index.js
@@ -30,11 +30,24 @@ router.post('/webhook', (req, res) => {
 
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
-
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      if (entry.messaging) {
+        entry.messaging.forEach(function(messagingObject) {
+          var senderId = messagingObject.sender.id;
+          if (messagingObject.message) {
+            var messageText = messagingObject.message.text;
+            messenger.sendTextMessage(senderId, messageText);
+          } else if (messagingObject.postback) {
+            console.log('Recieved postback');
+          }
+        });
+      } else {
+        console.log('no message key found');
+      }
     });
 
     // Returns a '200 OK' response to all requests
@@ -43,6 +56,77 @@ router.post('/webhook', (req, res) => {
     // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
+
+
+
+/*
+ console.log("\t >>>> Received request: " + JSON.stringify(req.body) );
+  var messaging = req.body.object;
+  
+  if (messaging ) {
+    if ( messaging.recipient.id != FB_PAGE_ID){
+      console.log("\t !!!-> Process not mine message !!!");    
+      return
+    }
+
+    const sender = messaging.sender.id;        
+    const sessionId = findOrCreateSession(sender);
+    const recipientId = sessions[sessionId].fbid;
+    console.log("\t ---> sender " + sender);
+    console.log("\t ---: sessionid " + sessionId);
+    console.log("\t ---: recipientId " + recipientId);
+    
+    if(sessions[sessionId].recipient == null || sessions[sessionId].recipient == undefined){
+      messenger.getProfile(recipientId, (err, data) => {
+        sessions[sessionId].recipient = data;
+        console.log("\t ---> user profile: " + JSON.stringify(sessions[sessionId].recipient));  
+      });      
+    }
+
+    console.log("\t ---> session context : " + JSON.stringify(sessions[sessionId].context));
+
+    var exceptional_case = false;
+    if(messaging.message && messaging.message.attachments && messaging.message.attachments.type == 'location'){
+      exceptional_case = true;
+    }
+
+    if(sessions[sessionId].location == {} && !exceptional_case){
+      messenger.sendQuickReplies(recipientId, "שלח לי בבקשה את איזור הגאוגראפי בו תרצה לבדוק מבצעים ;)" , [ { "content_type": "location" }]);
+    }
+    else if (messaging.message){
+      const atts = messaging.message.attachments;
+      const quick_reply = messaging.message.quick_reply;
+      
+      if( atts != null){       
+        attachment.respond(messenger, recipientId, atts, sessions[sessionId].context); 
+      }
+      else if( quick_reply != null){         
+          console.log("\t ---> postback PAYLOAD action: " + messaging.message.text);
+          postback(quick_reply.payload, recipientId, sessions[sessionId].context, messenger, sessions[sessionId].recipient);        
+      }
+      else{
+          console.log("\t ---> wit text process: " + messaging.message.text);
+          wit.run_actions(messenger, sessionId, messaging.message.text, sessions);        
+      }
+    } 
+    else if (messaging.postback){
+      console.log("\t ---> postback action: " + messaging.postback.payload);      
+      postback(messaging.postback.payload, recipientId, sessions[sessionId].context, messenger, sessions[sessionId].recipient);  
+    } 
+    else {
+      console.log("\t ---> received message format is not supported !!!");
+      messenger.sendTextMessage(recipientId, "תודה על פניתך");
+    }    
+  } else {
+    console.log("\t ---> message is null or reciever of the message is not a FB page");
+  }
+  res.sendStatus(200);
+
+
+*/
+
+
+
 
 });
 
