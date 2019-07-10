28f9e9bf1effe6af08b395b5bab88954ad56b7b0
alexperman <alex.perman@gmail.com>
2019-07-10 14:09:40 +0300

    FB integration #3

--- routes/index.js
+++ routes/index.js
@@ -5,130 +5,84 @@ var Store = require('../models/store');
 var Location = require('../models/location');
 
 
-const {Wit, log} = require('node-wit');
-const client = new Wit({
-  accessToken: "ZQMUMBSYZRXKHA4MSBM4Y7HMVXYXHTMF",
-  logger: new log.Logger(log.DEBUG) // optional
-});
+let Wit = null;
+let log = null;
+try {
+  // if running from repo
+  Wit = require('../').Wit;
+  log = require('../').log;
+} catch (e) {
+  Wit = require('node-wit').Wit;
+  log = require('node-wit').log;
+}
+
+const WIT_TOKEN = process.env.WIT_TOKEN || "ZQMUMBSYZRXKHA4MSBM4Y7HMVXYXHTMF";
 
 const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
 const FBMessenger = require('../middlewares/fb-messenger');
 const messenger = new FBMessenger(FB_PAGE_TOKEN);
 
+// Setting up our bot
+const wit = new Wit({
+  accessToken: WIT_TOKEN,
+  logger: new log.Logger(log.INFO)
+});
 
 router.get('/', function(req, res, next) {
 	res.render('homepage');
 });
 
+// Message handler
+router.post('/webhook', (req, res) => {
+  // Parse the Messenger payload
+  // See the Webhook reference
+  // https://developers.facebook.com/docs/messenger-platform/webhook-reference
+  const data = req.body;
 
-router.post('/webhook', (req, res) => {  
-  let body = req.body;
+  if (data.object === 'page') {
+    data.entry.forEach(entry => {
+      entry.messaging.forEach(event => {
+        if (event.message && !event.message.is_echo) {
+          // Yay! We got a new message!
+          // We retrieve the Facebook user ID of the sender
+          const sender = event.sender.id;
 
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
+          // We could retrieve the user's current session, or create one if it doesn't exist
+          // This is useful if we want our bot to figure out the conversation history
+          // const sessionId = findOrCreateSession(sender);
 
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+          // We retrieve the message content
+          const {text, attachments} = event.message;
 
-      if (entry.messaging) {
-        entry.messaging.forEach(function(messagingObject) {
-          var senderId = messagingObject.sender.id;
-          if (messagingObject.message) {
-            var messageText = messagingObject.message.text;
-            messenger.sendTextMessage(senderId, messageText);
-          } else if (messagingObject.postback) {
-            console.log('Recieved postback');
+          if (attachments) {
+            // We received an attachment
+            // Let's reply with an automatic message            
+            messenger.sendTextMessage(sender, 'Sorry I can only process text messages for now.');
+            .catch(console.error);
+          } else if (text) {
+            // We received a text message
+            // Let's run /message on the text to extract some entities
+            wit.message(text).then(({entities}) => {
+              // You can customize your response to these entities
+              console.log(entities);
+              // For now, let's reply with another automatic message
+              messenger.sendTextMessage(sender, 'We have received your message: ${text}.');
+            })
+            .catch((err) => {
+              console.error('Oops! Got an error from Wit: ', err.stack || err);
+            })
           }
-        });
-      } else {
-        console.log('no message key found');
-      }
+        } else {
+          console.log('received event', JSON.stringify(event));
+        }
+      });
     });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
-
-
-/*
- console.log("\t >>>> Received request: " + JSON.stringify(req.body) );
-  var messaging = req.body.object;
-  
-  if (messaging ) {
-    if ( messaging.recipient.id != FB_PAGE_ID){
-      console.log("\t !!!-> Process not mine message !!!");    
-      return
-    }
-
-    const sender = messaging.sender.id;        
-    const sessionId = findOrCreateSession(sender);
-    const recipientId = sessions[sessionId].fbid;
-    console.log("\t ---> sender " + sender);
-    console.log("\t ---: sessionid " + sessionId);
-    console.log("\t ---: recipientId " + recipientId);
-    
-    if(sessions[sessionId].recipient == null || sessions[sessionId].recipient == undefined){
-      messenger.getProfile(recipientId, (err, data) => {
-        sessions[sessionId].recipient = data;
-        console.log("\t ---> user profile: " + JSON.stringify(sessions[sessionId].recipient));  
-      });      
-    }
-
-    console.log("\t ---> session context : " + JSON.stringify(sessions[sessionId].context));
-
-    var exceptional_case = false;
-    if(messaging.message && messaging.message.attachments && messaging.message.attachments.type == 'location'){
-      exceptional_case = true;
-    }
-
-    if(sessions[sessionId].location == {} && !exceptional_case){
-      messenger.sendQuickReplies(recipientId, "שלח לי בבקשה את איזור הגאוגראפי בו תרצה לבדוק מבצעים ;)" , [ { "content_type": "location" }]);
-    }
-    else if (messaging.message){
-      const atts = messaging.message.attachments;
-      const quick_reply = messaging.message.quick_reply;
-      
-      if( atts != null){       
-        attachment.respond(messenger, recipientId, atts, sessions[sessionId].context); 
-      }
-      else if( quick_reply != null){         
-          console.log("\t ---> postback PAYLOAD action: " + messaging.message.text);
-          postback(quick_reply.payload, recipientId, sessions[sessionId].context, messenger, sessions[sessionId].recipient);        
-      }
-      else{
-          console.log("\t ---> wit text process: " + messaging.message.text);
-          wit.run_actions(messenger, sessionId, messaging.message.text, sessions);        
-      }
-    } 
-    else if (messaging.postback){
-      console.log("\t ---> postback action: " + messaging.postback.payload);      
-      postback(messaging.postback.payload, recipientId, sessions[sessionId].context, messenger, sessions[sessionId].recipient);  
-    } 
-    else {
-      console.log("\t ---> received message format is not supported !!!");
-      messenger.sendTextMessage(recipientId, "תודה על פניתך");
-    }    
-  } else {
-    console.log("\t ---> message is null or reciever of the message is not a FB page");
   }
   res.sendStatus(200);
-
-
-*/
-
-
-
-
 });
+
+
 
 // Adds support for GET requests to our webhook
 router.get('/webhook', (req, res) => {
@@ -138,15 +92,12 @@ router.get('/webhook', (req, res) => {
   let challenge = req.query['hub.challenge'];
     
   // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-  
+  if (mode && token) {  
     // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === FB_VERIFY_TOKEN) {
-      
+    if (mode === 'subscribe' && token === FB_VERIFY_TOKEN) {      
       // Responds with the challenge token from the request
       console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    
+      res.status(200).send(challenge);    
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);      
