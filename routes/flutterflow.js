7afa8bb02b23a99d9c442b08633f33f33abf7042
alexperman <alex.perman@gmail.com>
2021-08-15 21:34:11 +0300

    stores API #1

--- routes/flutterflow.js
+++ routes/flutterflow.js
@@ -18,6 +18,36 @@ var geocoder = NodeGeocoder(options);
 var Store = require('../models/store');
 var Location = require('../models/location');
 
+const store_img = {
+  "tivtam": "https://supermarkets.s3.eu-central-1.amazonaws.com/tivtam_new.jpg",
+  "coop": "https://supermarkets.s3.eu-central-1.amazonaws.com/coop_new.gif",
+  "shufersal": "https://supermarkets.s3.eu-central-1.amazonaws.com/shufersal_new.gif",
+  "doralon": "https://supermarkets.s3.eu-central-1.amazonaws.com/doralon_new.gif",
+  "superpharm": "",
+  "victory": "https://supermarkets.s3.eu-central-1.amazonaws.com/victory_new.gif",
+  "mahsaneihashuk": "https://supermarkets.s3.eu-central-1.amazonaws.com/mahsneishuk_new.gif",
+  "mahsaneilahav": "https://supermarkets.s3.eu-central-1.amazonaws.com/mahsanei-lahav.jpg",
+  "osherad": "https://supermarkets.s3.eu-central-1.amazonaws.com/osherad_new.gif",
+  "mega": "https://supermarkets.s3.eu-central-1.amazonaws.com/mega_new.gif",
+  "hazihinam": "https://supermarkets.s3.eu-central-1.amazonaws.com/hazihinam_new.gif",
+  "keshettaamim": "https://supermarkets.s3.eu-central-1.amazonaws.com/keshettaamim_new.gif",
+  "ramilevy": "https://supermarkets.s3.eu-central-1.amazonaws.com/ramilevy_new.gif",
+  "superdush": "https://supermarkets.s3.eu-central-1.amazonaws.com/superdush_new.gif",
+  "yohananof": "https://supermarkets.s3.eu-central-1.amazonaws.com/yohananov_new.gif",
+  "ybitan": "https://supermarkets.s3.eu-central-1.amazonaws.com/ybitan_new.gif",
+  "freshmarket": "https://supermarkets.s3.eu-central-1.amazonaws.com/freshmarket_new.gif",
+  "zolvebegadol": "https://supermarkets.s3.eu-central-1.amazonaws.com/zolbegadol_new.gif",
+  "stopmarket": "https://supermarkets.s3.eu-central-1.amazonaws.com/stopmarket.jpg",
+  "salahdabah": "",
+  "kingstore": "https://supermarkets.s3.eu-central-1.amazonaws.com/kingstore_new.gif",
+  "bareket": "https://supermarkets.s3.eu-central-1.amazonaws.com/superbareket_new.jpg",
+  "newpharm": "",
+  "shukhair": "",
+  "edenteva": "https://supermarkets.s3.eu-central-1.amazonaws.com/edenteva_new.gif",
+  "barkol": "https://supermarkets.s3.eu-central-1.amazonaws.com/barkol_new.gif",
+}
+
+
 async function convertAddrees2Point(req) {
     console.log( req.params["full_address"])
     var point = "Israel," + ","+ req.params["full_address"]
@@ -81,11 +111,11 @@ function to_flutterflow(stores, point = {}, locations = []){
     
     var rStore = {
       "id": store.get("id"),
-      //"chain": chain.get("description"),
-      //"name": store.get("name"),
-      //"city": location.get("city"),
-      //"address": location.get("address"),
-      //"chain_img": chain.get("name"),
+      "chain": chain.get("description"),
+      "name": store.get("name"),
+      "city": location.get("city"),
+      "address": location.get("address"),
+      "chain_img": store_img[chain.get("name")],
       "discounts_no":  Math.round((Math.random() * (250 - 75) + 75)),
       "location": geo_location(location),
       "distance_in_metr": getDistance2Point(point, location),
